95b31baa1cc4656c6ea527f2e349d3556680ef25
valentga <gina.valentino88@gmail.com>
2024-03-27 13:56:06 -1000

    Add files via upload

---
+++ 05-pdf/app/chat/vector_stores/pinecone_interact.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[3]
+sys.path.append(str(path_root))
+import os 
+import pinecone
+from langchain.vectorstores.pinecone import Pinecone
+from app.chat.embeddings.openai import embeddings
+
+pinecone.init(
+    api_key=os.getenv("PINECONE_API_KEY"),
+    environment=os.getenv("PINECONE_ENV_NAME")
+)
+
+vector_store = Pinecone.from_existing_index(
+    os.getenv("PINECONE_INDEX_NAME"),
+    embeddings
+)
+
+def build_retriever(chat_args, k):
+    search_kwargs = {
+        "filter": { "pdf_id": chat_args.pdf_id },
+        "k": k
+    }
+    return vector_store.as_retriever(
+        search_kwargs=search_kwargs
+    )
