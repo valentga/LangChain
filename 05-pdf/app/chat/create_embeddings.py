dda57097f7601454ca5535f27dcfacfd7d3fef13
valentga <gina.valentino88@gmail.com>
2024-04-10 13:44:59 -1000

    Update create_embeddings.py for chroma

--- 05-pdf/app/chat/create_embeddings.py
+++ 05-pdf/app/chat/create_embeddings.py
@@ -4,7 +4,8 @@ path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from app.chat.vector_stores.pinecone_interact import vector_store
+#from app.chat.vector_stores.pinecone_interact import vector_store
+from app.chat.vector_stores.chroma_db import vector_store
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     """
