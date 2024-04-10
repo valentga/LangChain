ebcdd4dcd8be67b95ee4e819e2533c12741be005
valentga <gina.valentino88@gmail.com>
2024-04-10 13:42:38 -1000

    Updates to vector_store __init__.py for chroma

--- 05-pdf/app/chat/vector_stores/__init__.py
+++ 05-pdf/app/chat/vector_stores/__init__.py
@@ -1,8 +1,15 @@
-from .pinecone_interact import build_retriever
+#from .pinecone_interact import build_retriever
+from .chroma_db import build_retriever
 from functools import partial
 
+# retriever_map = {
+#     "pinecone_1": partial(build_retriever, k=1),
+#     "pinecone_2": partial(build_retriever, k=2),
+#     "pinecone_3": partial(build_retriever, k=3)
+# }
+
 retriever_map = {
-    "pinecone_1": partial(build_retriever, k=1),
-    "pinecone_2": partial(build_retriever, k=2),
-    "pinecone_3": partial(build_retriever, k=3)
+    "chroma_1": partial(build_retriever, k=1),
+    "chroma_2": partial(build_retriever, k=2),
+    "chroma_3": partial(build_retriever, k=3)
 }
